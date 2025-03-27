ba97b2b93bcecab613378493ecffd078e2ad52a2
Wiktor Jaworski <wijaweq@gmail.com>
2025-03-27 20:25:16 +0100

    Fix missing required fields in openapi schema definitions

--- Application/DTOs/UserDto.cs
+++ Application/DTOs/UserDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Application.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs;
 
 /// <summary>
 /// Represents the data transfer object used for returning user data in API responses.
@@ -6,4 +8,7 @@
 /// <param name="Id">The unique identifier of the user.</param>
 /// <param name="Nickname">The nickname of the user.</param>
 /// <param name="Email">The email address of the user.</param>
-public record UserDto(int Id, string Nickname, string Email);
+public record UserDto(
+    [property: Required] int Id,
+    [property: Required] string Nickname,
+    [property: Required] string Email);
