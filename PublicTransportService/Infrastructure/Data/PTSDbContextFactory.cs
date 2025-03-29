043a85f92ff6440533632f84dd2fd7d9a93c9fc1
Lokinado <krzysztofborowski02@gmail.com>
2025-03-29 23:12:19 +0100

    Update PTSDbContextFactory

--- PublicTransportService/Infrastructure/Data/PTSDbContextFactory.cs
+++ PublicTransportService/Infrastructure/Data/PTSDbContextFactory.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-
-namespace PublicTransportService.Infrastructure.Data;
+using PublicTransportService.Infrastructure.Data;
 
 /// <summary>
 /// Represents a database context factory for the public transport system.
@@ -22,12 +21,29 @@ public class PTSDbContextFactory : IDesignTimeDbContextFactory<PTSDbContext>
         }
 
         var configuration = builder.AddEnvironmentVariables().Build();
-        var connectionString = configuration.GetConnectionString("AppDbConnectionString");
+        var connectionString = configuration.GetConnectionString("AppDbConnectionString")
+            ?? throw new InvalidOperationException("Missing connection string 'AppDbConnectionString'.");
+
+        var dbServerVersionRaw = Environment.GetEnvironmentVariable("DB_SERVER_VERSION");
+
+        ServerVersion serverVersion;
+        if (!string.IsNullOrWhiteSpace(dbServerVersionRaw))
+        {
+            serverVersion = ServerVersion.Parse(dbServerVersionRaw);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[WARN] Environment variable 'DB_SERVER_VERSION' not found. Falling back to ServerVersion.AutoDetect().");
+            Console.ResetColor();
+
+            serverVersion = ServerVersion.AutoDetect(connectionString);
+        }
 
         var optionsBuilder = new DbContextOptionsBuilder<PTSDbContext>()
             .UseMySql(
                 connectionString,
-                ServerVersion.AutoDetect(connectionString),
+                serverVersion,
                 mySqlOptions => mySqlOptions.MigrationsHistoryTable("__EFMigrationsHistory_PTS"))
             .UseSnakeCaseNamingConvention();
 
