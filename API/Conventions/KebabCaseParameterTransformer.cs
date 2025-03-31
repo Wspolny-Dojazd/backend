ef57d6896e6b6bff7a4a6ff7c9ddd50a8a89dc71
Pucha10 <puszka.dawid02@gmail.com>
2025-03-31 22:45:49 +0200

    Add kebab-case transformer

---
+++ API/Conventions/KebabCaseParameterTransformer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace API.Conventions;
+
+/// <summary>
+/// Transforms parameter names to kebab-case for use in route templates.
+/// </summary>
+public class KebabCaseParameterTransformer : IOutboundParameterTransformer
+{
+    /// <inheritdoc/>
+    public string? TransformOutbound(object? value)
+    {
+        return value != null
+            ? Regex.Replace(value.ToString()!, "([a-z])([A-Z])", "$1-$2").ToLower()
+            : null;
+    }
+}
