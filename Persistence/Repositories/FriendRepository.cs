6ec99e25692c5f1aab0d9e6e8072c6db1408a891
piecharka <kowalas02.kk@gmail.com>
2025-05-15 20:44:12 +0200

    Endpoint for retrieving logged in user's friends added

---
+++ Persistence/Repositories/FriendRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces;
+using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories;
+
+/// <summary>
+/// Represents crud operations for friendship relationships.
+/// </summary>
+/// <param name="databaseContext">The database context used to access group data.</param>
+public class FriendRepository(DatabaseContext databaseContext)
+    : IFriendRepository
+{
+    /// <inheritdoc/>
+    public async Task<IEnumerable<User>> GetAllAsync(Guid userId)
+    {
+        var user = await databaseContext.Users
+            .FirstOrDefaultAsync(u => u.Id == userId);
+
+        return user.Friends;
+    }
+}
