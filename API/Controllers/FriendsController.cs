db0d8854741dd8f786ce0af199df16f020d5ecb9
Kacper Kowalski <73427403+piecharka@users.noreply.github.com>
2025-05-21 21:27:59 +0200

    Add endpoint for retrieving user's friends (#92)

---
+++ API/Controllers/FriendsController.cs
@@ -0,0 +1,34 @@
+﻿using API.Extensions;
+using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Enums.ErrorCodes;
+
+namespace API.Controllers;
+
+/// <summary>
+/// Provides API endpoints for managing users' friendship relationships.
+/// </summary>
+/// <param name="friendService">The service that handles friend-related logic.</param>
+[Route("api/[controller]")]
+[ApiController]
+public class FriendsController(IFriendService friendService)
+    : ControllerBase
+{
+    /// <summary>
+    /// Retrieves a friend list for currently logged in user.
+    /// </summary>
+    /// <returns>The requested friend list details.</returns>
+    /// <response code="200">The friend list was successfully retrieved.</response>
+    /// <response code="404">The friend list was not found.</response>
+    [HttpGet]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse<UserErrorCode>), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetGroupById()
+    {
+        var userId = this.User.GetUserId();
+
+        var friends = await friendService.GetFriendsAsync(userId);
+        return this.Ok(friends);
+    }
+}
---
+++ Domain/Interfaces/IFriendRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Model;
+
+namespace Domain.Interfaces;
+
+/// <summary>
+/// Defines a contract for <see cref="User"/> Friends data access operations.
+/// </summary>
+public interface IFriendRepository
+{
+    /// <summary>
+    /// Retrieves a list of friends by user's unique identifier.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <returns>The friends of the specified user.</returns>
+    Task<IEnumerable<User>> GetAllAsync(Guid userId);
+}
