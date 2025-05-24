b71ad943992edfacba1bd29590647d05fbf35328
Dawid <127977551+Pucha10@users.noreply.github.com>
2025-05-24 21:22:07 +0200

    Add change nickname endpoint (#95)

---
+++ Application/DTOs/Auth/ChangeNicknameRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Auth;
+
+/// <summary>
+/// Represents the data transfer object used for nickname change requests.
+/// </summary>
+public class ChangeNicknameRequestDto
+{
+    /// <summary>
+    /// Gets the new nickname of the user.
+    /// </summary>
+    [Required]
+    public required string NewNickname { get; init; }
+}
